8b156c2548f62e7a4731d346e99a941679de3002
Kareem Awad <kareemawad120@gmail.com>
2025-05-25 22:53:08 +0300

    make package details v1

--- src/app/packageDetails/page.js
+++ src/app/packageDetails/page.js
@@ -1,8 +1,10 @@
+import DetailsWithSlider from "@/components/details-with-slider/details-with-slider";
 import KareemTest from "@/components/KareemTest";
+
 export default function PackageDetails() {
   return (
     <div>
-      <KareemTest />
+        <DetailsWithSlider />
     </div>
   );
 }
